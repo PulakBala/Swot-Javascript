425e8908848513464b74706055f398bdf3267179
PulakBala <pulakbala0524@gmail.com>
2022-08-05 11:24:56 +0600

    add switch case

---
+++ switch.js
@@ -0,0 +1,17 @@
+const color = 'green';
+switch(color) {
+    case 'red':
+        console.log('Your dress color red');
+        break;
+    case 'white':
+        console.log('Your dress color white');
+        break;
+    case 'blue':
+        console.log('Your dress color blue');
+        break;
+    case 'black':
+        console.log('Your dress color black')
+        break;
+    default:
+        console.log('Your dress color sadakala');                
+}
