46bdc4109d531b5fa1339d745ad2ae2eda892e92
PulakBala <pulakbala0524@gmail.com>
2022-08-09 09:25:09 +0600

    string indexof

---
+++ string-indexOf.js
@@ -0,0 +1,10 @@
+const lyrics = 'tmi bondhu kala pakhi ami jano ki . bosanto kale toami bolte pari ni'
+
+// console.log(lyrics.indexOf('janoi'));
+
+if(lyrics.indexOf('jano2') !== -1) {
+    console.log('exists inside the string')
+}
+else{
+    console.log('cannot find it');
+}
