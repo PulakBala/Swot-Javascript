2bcab3e7a9e39638716cabe84237e6df12176f20
PulakBala <pulakbala0524@gmail.com>
2022-08-09 12:16:53 +0600

    string split method add

---
+++ split.js
@@ -0,0 +1,4 @@
+const lyrics = "tmi bondhu kala pakhi ami jano ki , bosonto kale tomai valobasi ni";
+// console.log(lyrics.split(' '));
+// console.log(lyrics.split(''));
+console.log(lyrics.split('.'));
