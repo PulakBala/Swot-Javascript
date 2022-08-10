db7d13f7430bdfc90d26d2855441a40b719257e4
PulakBala <pulakbala0524@gmail.com>
2022-08-11 01:20:44 +0600

    shoping cart added

--- shopingcart.js
+++ shopingcart.js
@@ -1,15 +1,16 @@
 const shopingCart = [
-    {name:'shoe', price: 3300},
-    {name:'watch', price: 6300},
-    {name:'pant', price: 2400},
-    {name:'baelt', price: 300}
+    {name:'shoe', price: 3300, quantity:2},
+    {name:'watch', price: 6300, quantity:3},
+    {name:'pant', price: 2400, quantity:6},
+    {name:'baelt', price: 300, quantity:5}
 ];
 
 function totalCost(products) {
     let sum = 0;
     for(let i = 0; i < shopingCart.length; i++) {
         const product = products[i];
-        sum = sum + product.price;
+        const productAll = product.price * product.quantity;
+        sum = sum + productAll;
         
     }
     return sum;
